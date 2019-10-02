0567c97be3fb56368181dffb704714e7fcf0aae4
Dang Trinh <dang.tbh1623@gmail.com>
2019-10-02 18:16:38 +0200

    Neu

--- Hallo/src/Hallo.java
+++ Hallo/src/Hallo.java
@@ -2,8 +2,9 @@
 public class Hallo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated metaaaahod stub
 		System.out.print("abc");
+		System.out.print("xyz");
 	}
 
 }
